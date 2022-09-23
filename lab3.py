f1b7ac1aefea319f01bd37e7f3c3911cac1a0b80
Bogdanova Irina <113433209+finlik@users.noreply.github.com>
2022-09-23 21:16:35 +0500

    Добавлены комментарии

--- lab3.py
+++ lab3.py
@@ -1,21 +1,21 @@
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' #Удвоенный английский алфавит, если сдвиг будет большим
 print('Программа для шифрования и расширования сообщения методом Цезаря')
 x = int(input('Введите 1 для шифровки или 2 для расшифровки сообщения: '))
-if x == 1:
+if x == 1: #Функция шифровки
     step = int(input('Шаг шифровки: '))
     message = input('Введите сообщение на английском языке для шифровки: ').upper()
     result = ''
         
-    for i in message:
-        position = alphabet.find(i)
-        new_position = position + step
+    for i in message: #Поиск символа в алфавите
+        position = alphabet.find(i) #Находим место символа в списке
+        new_position = position + step #Сдвиг символа 
         if i in alphabet:
             result += alphabet[new_position]
         else:
             result += i 
     print ('Ваше зашифрованное сообщение:', result)
 
-elif x == 2:
+elif x == 2: #Функиция расшифровки
     step = int(input('Шаг шифровки: '))
     message = input('Введите сообщение на английском языке для расшифровке: ').upper()
     result = ''
@@ -28,5 +28,5 @@ elif x == 2:
             result += i
     print ('Ваше расшифрованное сообщениe:', result)
     
-else:
+else: #Если неправильно ввели команду
     print('Вы ввели неправильную команду. Повторите попытку.')
