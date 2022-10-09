3a39376b60a319c8f8e05624b272cfa92a972af2
Bogdanova Irina <113433209+finlik@users.noreply.github.com>
2022-10-09 15:24:58 +0500

    Интерфейс

--- lab3.py
+++ lab3.py
@@ -1,32 +1,44 @@
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' #Удвоенный английский алфавит, если сдвиг будет большим
 print('Программа для шифрования и расширования сообщения методом Цезаря')
-x = int(input('Введите 1 для шифровки или 2 для расшифровки сообщения: '))
-if x == 1: #Функция шифровки
-    step = int(input('Шаг шифровки: '))
-    message = input('Введите сообщение на английском языке для шифровки: ').upper()
-    result = ''
-        
-    for i in message: #Поиск символа в алфавите
-        position = alphabet.find(i) #Находим место символа в списке
-        new_position = position + step #Сдвиг символа 
-        if i in alphabet:
-            result += alphabet[new_position]
+while True:
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' #Удвоенный английский алфавит, если сдвиг будет большим
+    x = int(input('Введите 1 для шифровки или 2 для расшифровки сообщения: '))
+    if x == 1: #Функция шифровки
+        step = int(input('Введите шаг шифровки диапозоном с 1 до 25: '))
+        message = input('Введите сообщение на английском языке для шифровки: ').upper()
+        result = ''
+        if step > 25:
+            print ('Шаг превысил диапозон. Попробуйте еще раз.')
+            break
+        for i in message: #Поиск символа в алфавите
+            position = alphabet.find(i) #Находим место символа в списке
+            new_position = position + step #Сдвиг символа 
+            if i in alphabet:
+                result += alphabet[new_position]
+            else:
+                result += i 
+        if message = result:
+            print ('Вы ввели символ или текст не на английском языке. Попробуйте еще раз.')
         else:
-            result += i 
-    print ('Ваше зашифрованное сообщение:', result)
+            print ('Ваше зашифрованное сообщение:', result)
 
-elif x == 2: #Функиция расшифровки
-    step = int(input('Шаг шифровки: '))
-    message = input('Введите сообщение на английском языке для расшифровке: ').upper()
-    result = ''
-    for i in message:
-        position = alphabet.find(i)
-        new_position = position - step
-        if i in alphabet:
-            result += alphabet[new_position]
+    elif x == 2: #Функиция расшифровки
+        step = int(input('Введите шаг шифровки диапозоном с 1 до 25: '))
+        message = input('Введите сообщение на английском языке для расшифровке: ').upper()
+        result = ''
+        if step > 25:
+            print ('Шаг превысил диапозон. Попробуйте еще раз.')
+            break
+        for i in message:
+            position = alphabet.find(i)
+            new_position = position - step
+            if i in alphabet:
+                result += alphabet[new_position]
+            else:
+                result += i
+        if message == result:
+            print ('Вы ввели символ или текст не на английском языке. Попробуйте еще раз.')
         else:
-            result += i
-    print ('Ваше расшифрованное сообщениe:', result)
-    
-else: #Если неправильно ввели команду
-    print('Вы ввели неправильную команду. Повторите попытку.')
+            print ('Ваше зашифрованное сообщение:', result)
+    else: #Если неправильно ввели команду
+        print('Вы ввели неправильную команду. Повторите попытку.')
+    break
